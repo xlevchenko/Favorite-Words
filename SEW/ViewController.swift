c948b3ffdb6445cd0bf9fa519d245cdf0220d380
Olexsii <realcarter50@gmail.com>
2022-10-22 23:23:55 +0300

    Add gitignore

---
+++ SEW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SEW
+//
+//  Created by Olexsii Levchenko on 9/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
