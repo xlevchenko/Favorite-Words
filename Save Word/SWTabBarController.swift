874ffae62ebd4caa49731ec0b3720377b948018a
Olexsii <realcarter50@gmail.com>
2022-10-28 21:59:37 +0300

    Add TabBar Buttons

--- Save Word/SWTabBarController.swift
+++ Save Word/SWTabBarController.swift
@@ -12,7 +12,12 @@ class SWTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        viewControllers = [createHomeNavController()]
+        viewControllers = [
+            createHomeNavController(),
+            createAddListWordNavController(),
+            createLearnNavController(),
+            createProfileNavController(),
+        ]
     }
     
 
@@ -21,6 +26,27 @@ class SWTabBarController: UITabBarController {
         homeVC.title = "Home"
         homeVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), tag: 0)
         return UINavigationController(rootViewController: homeVC)
+    }
+    
+    func createAddListWordNavController() -> UINavigationController {
+        let addListWordVC = UIViewController()
+        addListWordVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "plus"), tag: 1)
+        addListWordVC.view.backgroundColor = .white
+        return UINavigationController(rootViewController: addListWordVC)
+    }
+    
+    func createLearnNavController() -> UINavigationController {
+        let learnVC = UIViewController()
+        learnVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "gamecontroller"), tag: 2)
+        learnVC.view.backgroundColor = .white
+        return UINavigationController(rootViewController: learnVC)
+    }
+    
+    func createProfileNavController() -> UINavigationController {
+        let profileVC = UIViewController()
+        profileVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person"), tag: 3)
+        profileVC.view.backgroundColor = .white
+        return UINavigationController(rootViewController: profileVC)
     }
 
 }
