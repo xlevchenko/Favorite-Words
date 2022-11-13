9b5c16ab22b646102f9dde9301e91bf2437d9087
Olexsii <realcarter50@gmail.com>
2022-11-13 17:36:45 +0200

    Update UI on Welcome Sceen

--- Save Word/WelcomeViewController.swift
+++ Save Word/WelcomeViewController.swift
@@ -24,89 +24,104 @@ class WelcomeViewController: UIViewController {
     private var welcomeLabel: UILabel = {
         let label = UILabel()
         label.text = "Welcome!"
-        label.font = .boldSystemFont(ofSize: 24)
+        label.font = .systemFont(ofSize: 36, weight: .heavy)
+        label.textColor = .white
         return label
     }()
     
     private var detaileLabel: UILabel = {
         let label = UILabel()
         label.text = "Log in with your data that you entered during Your registration."
-        label.font = .systemFont(ofSize: 18)
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .white
         label.numberOfLines = 0
         return label
     }()
     
     private var signUpButton: CustomButton = {
         let button = CustomButton()
-        button.setTitle("Sing Up", for: .normal)
-        button.setGradientColor(
-            colors: [UIColor(red: 0.31, green: 0.70, blue: 0.88, alpha: 1.00).cgColor,
-                     UIColor(red: 0.15, green: 0.48, blue: 0.75, alpha: 1.00).cgColor],
-            cornerRadius: 15)
+        button.setTitle("Sign Up", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = UIColor(red: 1.00, green: 0.79, blue: 0.24, alpha: 1.00)
+        button.layer.cornerRadius = 15
         return button
+    }()
+    
+    private var signInLabel = {
+        let label = UILabel()
+        label.text = "Already have an account!"
+        label.font = .systemFont(ofSize: 15, weight: .medium)
+        label.textColor = .white
+        return label
     }()
     
     private var singInButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Sing In", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.layer.cornerRadius = 15
-        button.layer.borderWidth = 1.5
-        button.layer.borderColor = CGColor(red: 0.24, green: 0.60, blue: 0.82, alpha: 1.00)
+        button.setTitle("Sign In", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        button.setTitleColor(UIColor(red: 1.00, green: 0.79, blue: 0.24, alpha: 1.00), for: .normal)
         return button
     }()
     
+    private var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 4
+        return stack
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(red: 0.43, green: 0.38, blue: 0.98, alpha: 1.00)
         
         view.addSubview(topImageContainerView)
         topImageContainerView.addSubview(welcomeImageView)
         view.addSubview(welcomeLabel)
         view.addSubview(detaileLabel)
         view.addSubview(signUpButton)
-        view.addSubview(singInButton)
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(signInLabel)
+        stackView.addArrangedSubview(singInButton)
         view.setNeedsUpdateConstraints()
     }
     
     override func updateViewConstraints() {
-            
-            topImageContainerView.snp.makeConstraints { make in
-                make.top.equalTo(view.snp.top).offset(30)
-                make.left.right.equalToSuperview()
-                make.height.equalTo(view.snp.height).multipliedBy(0.5)
-            }
-            
-            welcomeImageView.snp.makeConstraints { make in
-                make.centerX.equalTo(topImageContainerView.snp.centerX)
-                make.centerY.equalTo(topImageContainerView.snp.centerY).offset(50)
-                make.size.equalTo(CGSize(width: 331, height: 344)).multipliedBy(0.6)
-            }
-            
-            welcomeLabel.snp.makeConstraints { make in
-                make.top.equalTo(topImageContainerView.snp.bottom).offset(65)
-                make.left.right.equalToSuperview().offset(30)
-            }
-            
-            detaileLabel.snp.makeConstraints { make in
-                make.top.equalTo(welcomeLabel.snp.bottom).offset(10)
-                make.left.equalTo(view.snp.left).offset(30)
-                make.right.equalTo(view.snp.right).offset(-30.0)
-            }
-            
-            
-            signUpButton.snp.makeConstraints { make in
-                make.top.equalTo(detaileLabel.snp.bottom).offset(25)
-                make.centerX.equalTo(view.snp.centerX)
-                make.size.equalTo(CGSize(width: 280, height: 50))
-            }
-            
-            singInButton.snp.makeConstraints { make in
-                make.top.equalTo(signUpButton.snp.bottom).offset(15)
-                make.centerX.equalTo(signUpButton.snp.centerX)
-                make.size.equalTo(signUpButton)
-            }
+        
+        topImageContainerView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top).offset(30)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(view.snp.height).multipliedBy(0.5)
+        }
+        
+        welcomeImageView.snp.makeConstraints { make in
+            make.centerX.equalTo(topImageContainerView.snp.centerX)
+            make.centerY.equalTo(topImageContainerView.snp.centerY).offset(50)
+            make.size.equalTo(CGSize(width: 331, height: 344)).multipliedBy(0.6)
+        }
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.top.equalTo(topImageContainerView.snp.bottom).offset(65)
+            make.left.right.equalToSuperview().offset(30)
+        }
+        
+        detaileLabel.snp.makeConstraints { make in
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(10)
+            make.left.equalTo(view.snp.left).offset(30)
+            make.right.equalTo(view.snp.right).offset(-30.0)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.top.equalTo(detaileLabel.snp.bottom).offset(25)
+            make.centerX.equalTo(view.snp.centerX)
+            make.size.equalTo(CGSize(width: 280, height: 50))
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(signUpButton.snp.bottom).offset(12)
+            make.centerX.equalTo(view.snp.centerX)
+            //make.size.equalTo(signUpButton)
+        }
         super.updateViewConstraints()
     }
 }
